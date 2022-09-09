fed1467e95657e42a754c24f8eb573b24923906d
Jérôme FOULET <Gom3.78@Gmail.Com>
2022-09-10 01:23:42 +0200

    feat(reflexion): get data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+
+/* import {
+  USER_MAIN_DATA,
+  USER_ACTIVITY,
+  USER_AVERAGE_SESSIONS,
+  USER_PERFORMANCE,
+} from "./assets/mockedData/data"; */
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  /* console.log(
+    USER_MAIN_DATA,
+    USER_ACTIVITY,
+    USER_AVERAGE_SESSIONS,
+    USER_PERFORMANCE
+  ); */
+
+  // Use useSearchParams hook to get query parameters
+  const [searchParams] = useSearchParams();
+  // Get userId from query parameters and set it to 12 if not specified.
+  const userId = searchParams.get("userId") ?? 12;
+  // Get isMockedData from query parameters and set it to true by default - Only use true or false in query.
+  const isMockedData = !(searchParams.get("isMockedData") === "false");
+  //console.log(typeof isMockedData);
+  console.log("userId: %s", userId);
+  console.log("isMoackedData: %s", isMockedData.toString());
+
+  return <div className="App"></div>;
 }
 
 export default App;
